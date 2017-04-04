1ed10bbb226524e16044526bbae1a986a1c995be
himani-sharma <himani.sharma@weboniselab.com>
2017-04-04 12:50:37 +0530

    Added Session Storage

---
+++ ExampleApp/app/store/Contact.js
@@ -0,0 +1,13 @@
+Ext.define('ExampleApp.store.Contact', {
+    extend: 'Ext.data.Store',
+    model: 'ExampleApp.model.Contact',
+    // autoLoad: true,
+    config: {
+    	proxy: {
+     	//use sessionstorage if need to save data for that
+    	//specific session only
+     		type: 'sessionstorage',
+        	id  : 'name'
+    	}
+    }
+});
--- ExampleApp/app/view/Main.js
+++ ExampleApp/app/view/Main.js
@@ -10,7 +10,9 @@ Ext.define('ExampleApp.view.Main', {
     views : [
         'HomeView'
     ],
-    
+    stores : [
+        'ExampleApp.store.Contact' //it must be Contacts
+    ],
 
     config: {
         tabBarPosition: 'bottom',
@@ -57,9 +59,10 @@ Ext.define('ExampleApp.view.Main', {
                             handler: function(button, event) {
                                 var errorString = '';
                                 var form = button.up('formpanel');
+                                var values = form.getValues();
                                 
                                 // dump form fields into new model instance
-                                var model = Ext.create("ExampleApp.model.Contact", form.getValues());
+                                var model = Ext.create("ExampleApp.model.Contact", values);
                              
                                 // validate form fields
                                 var errors = model.validate();
@@ -72,7 +75,15 @@ Ext.define('ExampleApp.view.Main', {
                                     });
                                     Ext.Msg.alert('Errors in your input', errorString);
                                 } else {
-                                    // Ext.Msg.alert("Data is valid","Success");                                    
+                                    
+                                    Ext.Msg.alert("Data is valid","Success");                              
+                                    var newRecord = new ExampleApp.model.Contact(values);                                                                                                        
+                                    var store = Ext.create('ExampleApp.store.Contact', { model: "ExampleApp.model.Contact" }); 
+                                      
+                                    store.load();
+                                    store.add(newRecord);
+                                    store.sync();
+                                   
                                     Ext.Viewport.down('main').destroy();
                                     Ext.Viewport.add(Ext.create('ExampleApp.view.Home', { fullscreen: true }));
                             }                           
